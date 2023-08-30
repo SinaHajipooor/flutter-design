36601f29f89ce0bc30cb576212bd7a38efc19126
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-08-30 13:51:06 +0330

    create the otp screen structure

---
+++ lib/screens/auth/otp_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class OtpScreen extends StatefulWidget {
+  static const souteName = '/otp-screen';
+  const OtpScreen({super.key});
+
+  @override
+  State<OtpScreen> createState() => _OtpScreenState();
+}
+
+class _OtpScreenState extends State<OtpScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/services/routes/app_routes.dart
+++ lib/services/routes/app_routes.dart
@@ -1,6 +1,9 @@
+import 'package:testui/screens/auth/otp_screen.dart';
+
 import '../imports/app_imports.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
   UserPassLoginScreen.routeName: (ctx) => const UserPassLoginScreen(),
   PhoneLoginScreen.routeName: (ctx) => const PhoneLoginScreen(),
+  OtpScreen.souteName: (ctx) => const OtpScreen(),
 };
